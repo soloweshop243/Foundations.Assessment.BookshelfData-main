0d84d920e291bcb1e71bbb834f1b4081b117b89c
soloweshop243 <122837682+soloweshop243@users.noreply.github.com>
2023-02-26 18:43:47 -0500

    dubugging remove book now works

--- js/index.js
+++ js/index.js
@@ -129,18 +129,29 @@ class BookList {
   addBook(book) {
     this.books.push(book);
   }
+  removeBook(book) {
+    const index = this.books.indexOf(book);
+    if (index !== -1) {
+      this.books.splice(index, 1);
+    }
+  }
+
 }
 
-// Create some book objects
+// Create some dummy book objects
 const myBook1 = {title: 'Book 1', author: 'Author 1', subject: 'Subject 1', language: 'English'};
 const myBook2 = {title: 'Book 2', author: 'Author 2', subject: 'Subject 2', language: 'French'};
 const myBook3 = {title: 'Book 3', author: 'Author 3', subject: 'Subject 3', language: 'Spanish'};
 
 // Initialize the book list and add the books
-const bookList = new BookList();
-bookList.addBook(myBook1);
-bookList.addBook(myBook2);
-bookList.addBook(myBook3);
+const bookList = new BookList()
+bookList.addBook(myBook1)
+bookList.addBook(myBook2)
+bookList.addBook(myBook3)
+
+console.log(bookList)
+bookList.removeBook(myBook1)
+console.log(bookList)
 
 
 // let myBook1= new Book( 'The JungleBook', 'Steve', 'Fiction', 'en', 'love')
